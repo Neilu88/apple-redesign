20eb2e5e37b629996211f7ac9f98c3c110e7fb92
Neil <neild2002@gmail.com>
2022-12-08 18:48:18 -0500

    initial header design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,27 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import logo from '../public/logo.png'
 
 function Header() {
   return (
-    <header className="">
-      <div className="relative h-20 w-20 cursor-pointer opacity-75 hover:opacity-50 transition select-none">
-        <Image src="https://logopond.com/logos/0c283734b9d338384b1264ce6afdeb68.png" layout="fill" objectFit="contain" alt="logo" draggable="false"/>
+    <header className="sticky top-0 flex items-center w-full justify-between z-30 bg-[#E7ECEE]">
+
+      <div className="md:w-1/5">
+        <Link href="/">
+          <div className="relative h-12 w-12 cursor-pointer opacity-75 hover:opacity-100 transition select-none">
+            <Image src={logo} layout="fill" objectFit="contain" alt="logo" draggable="false"/>
+          </div>
+        </Link>
       </div>
+
+      <div className="flex space-x-8 flex-1">
+        <a className="headerLink">Products</a>
+        <a className="headerLink">Explore</a>
+        <a className="headerLink">Support</a>
+        <a className="headerLink">Buy</a>
+      </div>
+
     </header>
   )
 }
