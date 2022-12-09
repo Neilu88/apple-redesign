66a9a9746fe66054366c146342b27f4a69e4fc66
Neil <neild2002@gmail.com>
2022-12-09 05:31:40 -0500

    initial header fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,9 @@ import logo from '../public/logo.png'
 
 function Header() {
   return (
-    <header className="sticky top-0 flex items-center w-full justify-between z-30 bg-[#E7ECEE]">
+    <header className="p-2 sticky top-0 flex items-center w-full justify-between z-30 bg-[#E7ECEE]">
 
-      <div className="md:w-1/5">
+      <div className="flex md:w-1/5 items-center justify-center">
         <Link href="/">
           <div className="relative h-12 w-12 cursor-pointer opacity-75 hover:opacity-100 transition select-none">
             <Image src={logo} layout="fill" objectFit="contain" alt="logo" draggable="false"/>
@@ -15,7 +15,7 @@ function Header() {
         </Link>
       </div>
 
-      <div className="flex space-x-8 flex-1">
+      <div className="hidden md:flex items-center justify-center space-x-8 flex-1">
         <a className="headerLink">Products</a>
         <a className="headerLink">Explore</a>
         <a className="headerLink">Support</a>
