332946cfd3d55e8d9bce461f40fc4ff1c4be0ba1
Neil <neild2002@gmail.com>
2022-12-13 04:20:27 -0500

    redid header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,23 +3,42 @@ import Link from 'next/link'
 import React from 'react'
 import logo from '../public/logo.png'
 
+import { SearchIcon, ShoppingBagIcon, UserIcon} from '@heroicons/react/outline'
+
 function Header() {
   return (
-    <header className="p-2 sticky top-0 flex items-center w-full justify-between z-30 bg-[#E7ECEE]">
+    <header className="sticky top-0 z-30 p-4 flex items-center justify-between bg-[#E7ECEE] w-full">
 
-      <div className="flex md:w-1/5 items-center justify-center">
+      <div className="flex items-center justify-center md:w-1/5">
         <Link href="/">
-          <div className="relative h-12 w-12 cursor-pointer opacity-75 hover:opacity-100 transition select-none">
-            <Image src={logo} layout="fill" objectFit="contain" alt="logo" draggable="false"/>
+          <div className="relative cursor-pointer w-14 h-14 select-none opacity-75 hover:opacity-100">
+            <Image src={logo} className="object-contain fill" alt="logo" fill={true} draggable={false} />
           </div>
         </Link>
       </div>
 
-      <div className="hidden md:flex items-center justify-center space-x-8 flex-1">
+      <div className="flex-1 hidden md:flex justify-center items-center space-x-8">
         <a className="headerLink">Products</a>
         <a className="headerLink">Explore</a>
         <a className="headerLink">Support</a>
-        <a className="headerLink">Buy</a>
+        <a className="headerLink">Business</a>
+      </div>
+
+      <div className="flex items-center justify-center space-x-4 md:w-1/5">
+
+        <SearchIcon className="headerIcon" />
+
+        <Link href="/checkout">
+          <div className="relative cursor-pointer select-none">
+            <span className="z-50 absolute h-4 w-4 flex items-center justify-center rounded-full -top-1 -right-1 bg-gradient-to-r from-pink-500 to-purple-500">
+              5
+            </span>
+            <ShoppingBagIcon className="headerIcon" />
+          </div>
+        </Link>
+        
+        <UserIcon className="headerIcon" />
+
       </div>
 
     </header>
